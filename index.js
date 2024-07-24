daaf7ec80dc9b3cc54c65b88eb29f42d83714da0
Mehran <mehranbhat2001@gmail.com>
2024-07-24 14:45:41 +0530

    ip

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-let user = {
-  name: "mehran",
-  age: 34,
-};
-let car = {
-  model: 2019,
-  cname: "mustang",
-};
+const numbs = [1, 60, 38, 77, 45, 9, 78, 37, 38, 82, 29];
+const res = numbs.filter((numbs) => numbs > 50);
+console.log(res);
 
-const comb = { user, car };
-console.log(comb.user);
+const names = [
+  "mehran",
+  "uzair",
+  "ali",
+  "aqib",
+  "muzzaffar",
+  "saim",
+  "zahid",
+  "rizz",
+  "rani",
+];
+const ress = names.filter((names) => names.endsWith("i"));
+console.log(ress);
