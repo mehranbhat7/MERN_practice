b6c63321258568c1c6ff9075434b298675a74822
Mehran <mehranbhat2001@gmail.com>
2024-07-30 11:46:49 +0530

    js practice day1

--- index.js
+++ index.js
@@ -1,92 +1,137 @@
-const colorPicker = document.getElementById("textcolor");
-const canvasColor = document.getElementById("background");
-const canvas = document.getElementById("canvas");
-const undoButton = document.getElementById("retrieve");
-const clearButton = document.getElementById("clear");
-const saveButton = document.getElementById("save");
-const fontPicker = document.getElementById("fontsize");
+// const colorPicker = document.getElementById("textcolor");
+// const canvasColor = document.getElementById("background");
+// const canvas = document.getElementById("canvas");
+// const undoButton = document.getElementById("retrieve");
+// const clearButton = document.getElementById("clear");
+// const saveButton = document.getElementById("save");
+// const fontPicker = document.getElementById("fontsize");
 
-const ctx = canvas.getContext("2d");
+// const ctx = canvas.getContext("2d");
 
-// Ensure the canvas is properly sized
-canvas.width = canvas.offsetWidth;
-canvas.height = canvas.offsetHeight;
+// // Ensure the canvas is properly sized
+// canvas.width = canvas.offsetWidth;
+// canvas.height = canvas.offsetHeight;
 
-let isDrawing = false;
-let lastX = 0;
-let lastY = 0;
+// let isDrawing = false;
+// let lastX = 0;
+// let lastY = 0;
 
-// Set default font size
-ctx.lineWidth = fontPicker.value;
+// // Set default font size
+// ctx.lineWidth = fontPicker.value;
 
-colorPicker.addEventListener("change", (event) => {
-  ctx.strokeStyle = event.target.value;
-  ctx.fillStyle = event.target.value;
-});
+// colorPicker.addEventListener("change", (event) => {
+//   ctx.strokeStyle = event.target.value;
+//   ctx.fillStyle = event.target.value;
+// });
 
-canvasColor.addEventListener("change", (event) => {
-  ctx.fillStyle = event.target.value;
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-});
+// canvasColor.addEventListener("change", (event) => {
+//   ctx.fillStyle = event.target.value;
+//   ctx.fillRect(0, 0, canvas.width, canvas.height);
+// });
 
-fontPicker.addEventListener("change", (event) => {
-  ctx.lineWidth = event.target.value;
-});
+// fontPicker.addEventListener("change", (event) => {
+//   ctx.lineWidth = event.target.value;
+// });
 
-canvas.addEventListener("mousedown", (event) => {
-  isDrawing = true;
-  [lastX, lastY] = [event.offsetX, event.offsetY];
-});
+// canvas.addEventListener("mousedown", (event) => {
+//   isDrawing = true;
+//   [lastX, lastY] = [event.offsetX, event.offsetY];
+// });
 
-canvas.addEventListener("mousemove", (event) => {
-  if (!isDrawing) return;
-  ctx.beginPath();
-  ctx.moveTo(lastX, lastY);
-  ctx.lineTo(event.offsetX, event.offsetY);
-  ctx.stroke();
-  [lastX, lastY] = [event.offsetX, event.offsetY];
-});
+// canvas.addEventListener("mousemove", (event) => {
+//   if (!isDrawing) return;
+//   ctx.beginPath();
+//   ctx.moveTo(lastX, lastY);
+//   ctx.lineTo(event.offsetX, event.offsetY);
+//   ctx.stroke();
+//   [lastX, lastY] = [event.offsetX, event.offsetY];
+// });
 
-canvas.addEventListener("mouseup", () => {
-  isDrawing = false;
-});
+// canvas.addEventListener("mouseup", () => {
+//   isDrawing = false;
+// });
 
-canvas.addEventListener("contextmenu", (event) => {
-  event.preventDefault();
-});
+// canvas.addEventListener("contextmenu", (event) => {
+//   event.preventDefault();
+// });
 
-clearButton.addEventListener("click", () => {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-});
+// clearButton.addEventListener("click", () => {
+//   ctx.clearRect(0, 0, canvas.width, canvas.height);
+// });
 
-saveButton.addEventListener("click", () => {
-  const image = canvas
-    .toDataURL("image/png")
-    .replace("image/png", "image/octet-stream");
-  const link = document.createElement("a");
-  link.download = "signature.png";
-  link.href = image;
-  link.click();
-});
+// saveButton.addEventListener("click", () => {
+//   const image = canvas
+//     .toDataURL("image/png")
+//     .replace("image/png", "image/octet-stream");
+//   const link = document.createElement("a");
+//   link.download = "signature.png";
+//   link.href = image;
+//   link.click();
+// });
 
-undoButton.addEventListener("click", () => {
-  const savedCanvas = localStorage.getItem("canvasContents");
-  if (savedCanvas) {
-    const img = new Image();
-    img.src = savedCanvas;
-    img.onload = () => {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      ctx.drawImage(img, 0, 0);
-    };
-  }
-});
+// undoButton.addEventListener("click", () => {
+//   const savedCanvas = localStorage.getItem("canvasContents");
+//   if (savedCanvas) {
+//     const img = new Image();
+//     img.src = savedCanvas;
+//     img.onload = () => {
+//       ctx.clearRect(0, 0, canvas.width, canvas.height);
+//       ctx.drawImage(img, 0, 0);
+//     };
+//   }
+// });
 
-// Save canvas contents to local storage
-saveButton.addEventListener("click", () => {
-  localStorage.setItem("canvasContents", canvas.toDataURL());
-  // Trigger download
-  const link = document.createElement("a");
-  link.download = "signature.png";
-  link.href = canvas.toDataURL();
-  link.click();
-});
+// // Save canvas contents to local storage
+// saveButton.addEventListener("click", () => {
+//   localStorage.setItem("canvasContents", canvas.toDataURL());
+//   // Trigger download
+//   const link = document.createElement("a");
+//   link.download = "signature.png";
+//   link.href = canvas.toDataURL();
+//   link.click();
+// });
+
+// js practice MERN
+
+let katora1 = "Rajma dal";
+let katora2 = katora1;
+console.log(katora2);
+
+var address = "nawakadal";
+var address = "lalbazar";
+console.log(address);
+
+// let name = "mehran";
+// let name = "ali";
+// console.log(name);
+
+function user() {
+  let age = 10;
+  console.log("hello");
+}
+user();
+console.log(age);
+
+let age = 23;
+console.log(typeof age);
+
+let name = "mehran";
+console.log(typeof name);
+
+let male = true;
+console.log(typeof male);
+
+let ress = null;
+console.log(ress);
+
+let eidi;
+console.log(typeof eidi);
+
+let num = "3" - null;
+console.log(num);
+console.log(typeof num);
+
+let res = "mehran";
+console.log(res);
+res = Boolean(res);
+console.log(typeof res);
