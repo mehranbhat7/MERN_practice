bcbd8fa77a56ec15d2f5f839cb1067371af258db
Mehran <mehranbhat2001@gmail.com>
2024-09-09 11:54:13 +0530

    strings DSA

--- index.js
+++ index.js
@@ -1417,29 +1417,7 @@
 // let res = first.slice(0, i);
 // console.log(res === "" ? -1 : res);
 
-function inp(val) {
-  const romans = [
-    { value: 1000, symbol: "M" },
-    { value: 900, symbol: "CM" },
-    { value: 500, symbol: "D" },
-    { value: 400, symbol: "CD" },
-    { value: 100, symbol: "C" },
-    { value: 90, symbol: "XC" },
-    { value: 50, symbol: "L" },
-    { value: 40, symbol: "XL" },
-    { value: 10, symbol: "X" },
-    { value: 9, symbol: "IX" },
-    { value: 5, symbol: "V" },
-    { value: 4, symbol: "IV" },
-    { value: 1, symbol: "I" },
-  ];
-  let total = "";
-  for (let { value, symbol } of romans) {
-    while (val >= value) {
-      total += symbol;
-      val -= value;
-    }
-  }
-  return total;
-}
-console.log(inp(187));
+let srt = " my name is mehran muzaffar bhat";
+let str = srt.replaceAll("m", "z");
+console.log(srt);
+console.log(str);
