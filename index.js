762bf231403808f0d7a6dc4b5f34f95b909af209
Mehran <mehranbhat2001@gmail.com>
2024-09-09 11:37:21 +0530

    string DSA

--- index.js
+++ index.js
@@ -1417,42 +1417,29 @@
 // let res = first.slice(0, i);
 // console.log(res === "" ? -1 : res);
 
-const romanHash = {
-  I: 1,
-  V: 5,
-  X: 10,
-  L: 50,
-  C: 100,
-  D: 500,
-  M: 1000,
-};
-const s = "MCMLXXXIX";
-// s = 1989
-function romanToInt(s) {
-  let accumulator = 0;
-  for (let i = 0; i < s.length; i++) {
-    if (s[i] === "I" && s[i + 1] === "V") {
-      accumulator += 4;
-      i++;
-    } else if (s[i] === "I" && s[i + 1] === "X") {
-      accumulator += 9;
-      i++;
-    } else if (s[i] === "X" && s[i + 1] === "L") {
-      accumulator += 40;
-      i++;
-    } else if (s[i] === "X" && s[i + 1] === "C") {
-      accumulator += 90;
-      i++;
-    } else if (s[i] === "C" && s[i + 1] === "D") {
-      accumulator += 400;
-      i++;
-    } else if (s[i] === "C" && s[i + 1] === "M") {
-      accumulator += 900;
-      i++;
-    } else {
-      accumulator += romanHash[s[i]];
+function inp(val) {
+  const romans = [
+    { value: 1000, symbol: "M" },
+    { value: 900, symbol: "CM" },
+    { value: 500, symbol: "D" },
+    { value: 400, symbol: "CD" },
+    { value: 100, symbol: "C" },
+    { value: 90, symbol: "XC" },
+    { value: 50, symbol: "L" },
+    { value: 40, symbol: "XL" },
+    { value: 10, symbol: "X" },
+    { value: 9, symbol: "IX" },
+    { value: 5, symbol: "V" },
+    { value: 4, symbol: "IV" },
+    { value: 1, symbol: "I" },
+  ];
+  let total = "";
+  for (let { value, symbol } of romans) {
+    while (val >= value) {
+      total += symbol;
+      val -= value;
     }
   }
-  return accumulator;
+  return total;
 }
-console.log(romanToInt("XXX"));
+console.log(inp(187));
